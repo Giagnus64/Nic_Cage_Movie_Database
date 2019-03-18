e6488ac8b2bb86052249ba5c59259eb961662270
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-03-18 16:48:48 -0400

    Finalized styles, added 404 Page

--- middleware/index.js
+++ middleware/index.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const middleWareObj = {};
 
+
+
 middleWareObj.handler404 = function(req, res, next){
-    res.status(404).send("Page does not exist. Please visit the home page here:<homepageUrl>");
+    res.status(404).redirect("404.html");
 };
 
 //handler for Error 500
 middleWareObj.handler500 = function(err, req, res, next){
     console.error(err.stack);
-    res.send('This is the 500 page');
+    res.send('This is the 500 page.');
 };
 
 module.exports = middleWareObj;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Movie = require('../models/movie');
 
 
 router.get('/api', (req,res) =>{
     res.json({
         status: 'API is currently working!',
-        message: "You've reached the base page of the api. Please search using the parameters defined at <homepageUrl>"
+        message: "You've reached the base page of the api. Please search using the parameters defined at the home page."
 
     });
 });
